d62f03f77121ce659039466da92bf3ccdb06927f
charles-hefele <90722369+charles-hefele@users.noreply.github.com>
2021-04-27 22:54:48 -0400

    updates to digger random agent
    Signed-off-by: charles-hefele <90722369+charles-hefele@users.noreply.github.com>

--- gym_digger_qnet.py
+++ gym_digger_qnet.py
@@ -5,18 +5,24 @@ from IPython.display import clear_output
 
 env = gym.make('Digger-v0')
 
-for ep in range(100):
+for episode in range(5):
     env.reset()
     done = False
+    step = 0
+    total_reward = 0
     while not done:
+        step += 1
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
-        nutrients = observation[0]
-        x_pos = observation[1]
-        y_pos = observation[2]
         battery = info['battery']
-        print(f'battery: {battery}')
+        total_reward += reward
 
+        # print stats
+        print(f'episode: {episode}, step: {step}, action: {action}, reward: {reward}, tot_reward: {total_reward}, done: {done}, battery: {battery}')
+
+        # render it
         env.render()
+
+        # delay
         time.sleep(0.05)
         clear_output(wait=True)
