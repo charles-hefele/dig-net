e58ef7c627b6d8e51b06b85eb7bd7fee1c6e2d00
charles-hefele <90722369+charles-hefele@users.noreply.github.com>
2021-05-01 00:16:54 -0400

    added support for wandb
    Signed-off-by: charles-hefele <90722369+charles-hefele@users.noreply.github.com>

--- gym_digger_dqn_keras-rl2.py
+++ gym_digger_dqn_keras-rl2.py
@@ -9,15 +9,18 @@ from tensorflow.keras.optimizers import Adam
 from rl.agents.dqn import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
-
+from rl.callbacks import WandbLogger, FileLogger
+import wandb
 
 ENV_NAME = 'Digger-v0'
+MAP_NAME = '2x2_e'
 
+wandb.init(project='digger',
+           group=MAP_NAME,
+           job_type='6-values')
 
 # Get the environment and extract the number of actions.
-env = gym.make(ENV_NAME)
-np.random.seed(123)
-env.seed(123)
+env = gym.make(ENV_NAME, map_name=MAP_NAME)
 nb_actions = env.action_space.n
 
 # Next, we build a very simple model.
@@ -44,11 +47,10 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-# dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
-dqn.fit(env, nb_steps=2000, visualize=False, verbose=2)
+dqn.fit(env, nb_steps=50000, visualize=False, verbose=2, callbacks=[WandbLogger(), FileLogger('results.json')])
 
 # After training is done, we save the final weights.
-dqn.save_weights(f'dqn_{ENV_NAME}_weights.h5f', overwrite=True)
+dqn.save_weights(f'dqn_{ENV_NAME}_{MAP_NAME}_weights.h5f', overwrite=True)
 
-# Finally, evaluate our algorithm for 5 episodes.
-dqn.test(env, nb_episodes=5, visualize=True)
+# Finally, evaluate our algorithm.
+dqn.test(env, nb_episodes=1, visualize=True)
