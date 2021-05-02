c3a72329cc55c46e63d670be40a8ad29664f7826
charles-hefele <90722369+charles-hefele@users.noreply.github.com>
2021-05-02 16:07:44 -0400

    comments refactoring
    Signed-off-by: charles-hefele <90722369+charles-hefele@users.noreply.github.com>

--- gym_digger_dqn_keras-rl2.py
+++ gym_digger_dqn_keras-rl2.py
@@ -38,7 +38,7 @@ wandb.init(project=PROJECT,
            job_type=JOB_TYPE,
            settings=wandb.Settings(start_method="thread"))
 
-# Print the params
+# print the params
 print(f'ENV_NAME: {ENV_NAME}, MAP_NAME: {MAP_NAME}, BATTERY: {BATTERY}, COMPLETION_BONUS: {COMPLETION_BONUS}, '
       f'BATTERY_PENALTY: {BATTERY_PENALTY}')
 
@@ -47,12 +47,12 @@ print(f'SEQUENTIAL_MEMORY: {SEQUENTIAL_MEMORY}, WINDOW_LENGTH: {WINDOW_LENGTH}, 
 
 print(f'PROJECT: {PROJECT}, GROUP: {GROUP}, JOB_TYPE: {JOB_TYPE}')
 
-# Get the environment and extract the number of actions.
+# build the environment
 env = gym.make(ENV_NAME, map_name=MAP_NAME, battery=BATTERY, completion_bonus=COMPLETION_BONUS,
                battery_penalty=BATTERY_PENALTY)
 nb_actions = env.action_space.n
 
-# Next, we build a very simple model.
+# build the model
 model = Sequential()
 model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
 model.add(Dense(16))
@@ -65,28 +65,25 @@ model.add(Dense(nb_actions))
 model.add(Activation('linear'))
 print(model.summary())
 
-# Finally, we configure and compile our agent. You can use every built-in tensorflow.keras optimizer and
-# even the metrics!
+# configure and compile the agent
 memory = SequentialMemory(limit=SEQUENTIAL_MEMORY, window_length=WINDOW_LENGTH)
 policy = BoltzmannQPolicy()
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=STEPS_WARMUP,
                target_model_update=TARGET_MODEL_UPDATE, policy=policy)
 dqn.compile(Adam(lr=LEARNING_RATE), metrics=['mae'])
 
-# Okay, now it's time to learn something! We visualize the training here for show, but this
-# slows down training quite a lot. You can always safely abort the training prematurely using
-# Ctrl + C.
+# train the agent
 dqn.fit(env, nb_steps=STEPS, visualize=False, verbose=2, callbacks=[WandbLogger(), FileLogger('results.json')])
 
-# After training is done, we save the final weights.
+# save the weights
 dqn.save_weights(f'dqn_{ENV_NAME}_{MAP_NAME}_weights.h5f', overwrite=True)
 
-# Render the initial board state
+# render the initial environment state
 env.reset()
 env.render()
 
-# Finally, evaluate our algorithm.
+# test the agent
 dqn.test(env, nb_episodes=1, visualize=True)
 
-# Terminate wandb
+# terminate wandb
 wandb.finish()
