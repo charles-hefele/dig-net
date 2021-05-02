2804cda6364303797ba81ccea7cf74340c7f7ceb
charles-hefele <90722369+charles-hefele@users.noreply.github.com>
2021-05-01 21:21:50 -0400

    pulled maps out into their own file
    Signed-off-by: charles-hefele <90722369+charles-hefele@users.noreply.github.com>

--- gym_digger/envs/digger_env.py
+++ gym_digger/envs/digger_env.py
@@ -4,90 +4,18 @@ import gym
 from gym import spaces
 from gym.utils import colorize
 from io import StringIO
+from .maps import MAPS
 
+# constants
 # BATTERY_LIFE = 200    # good for 2x2 and 3x3
 BATTERY_LIFE = 10000
 
+# actions
 LEFT = 0
 DOWN = 1
 RIGHT = 2
 UP = 3
 DIG = 4
-
-MAPS = {
-    '2x2_a': np.array([
-        [1, 1],
-        [1, 1]
-    ]),
-    '2x2_b': np.array([
-        [2, 1],
-        [1, 2]
-    ]),
-    '2x2_c': np.array([
-        [3, 1],
-        [0, 2]
-    ]),
-    '2x2_d': np.array([
-        [3, 4],
-        [4, 2]
-    ]),
-    '2x2_e': np.array([
-        [3, 5],
-        [4, 2]
-    ]),
-    '2x2_f': np.array([
-        [3, 6],
-        [4, 2]
-    ]),
-    '2x2_g': np.array([
-        [3, 7],
-        [4, 2]
-    ]),
-    '2x2_h': np.array([
-        [3, 8],
-        [4, 2]
-    ]),
-    '2x2_i': np.array([
-        [3, 9],
-        [4, 2]
-    ]),
-    '2x2_j': np.array([
-        [3, 10],
-        [4, 2]
-    ]),
-    '2x2_k': np.array([
-        [3, 25],
-        [4, 2]
-    ]),
-    '3x3_a': np.array([
-        [0, 0, 1],
-        [0, 1, 1],
-        [1, 1, 1]
-    ]),
-    '3x3_b': np.array([
-        [0, 2, 1],
-        [0, 1, 3],
-        [2, 3, 2]
-    ]),
-    '4x4_a': np.array([
-        [2, 2, 1, 3],
-        [1, 0, 0, 1],
-        [2, 0, 1, 1],
-        [1, 3, 1, 0]
-    ]),
-    '10x10': np.array([
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 3, 2, 0, 0, 0, 4, 0, 0],
-        [0, 0, 1, 2, 0, 0, 4, 5, 4, 0],
-        [0, 0, 0, 0, 0, 0, 0, 4, 0, 0],
-        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 2, 2, 2, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 3, 2, 0],
-        [0, 1, 2, 2, 0, 0, 3, 3, 2, 0],
-        [0, 0, 3, 3, 0, 0, 0, 2, 2, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ])
-}
 
 
 class DiggerEnv(gym.Env):
