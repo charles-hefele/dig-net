acb34a53d08699a5b16fd845e3767b9e5e934536
charles-hefele <90722369+charles-hefele@users.noreply.github.com>
2021-05-01 21:06:37 -0400

    extracted params
    Signed-off-by: charles-hefele <90722369+charles-hefele@users.noreply.github.com>

--- gym_digger_dqn_keras-rl2.py
+++ gym_digger_dqn_keras-rl2.py
@@ -14,10 +14,18 @@ import wandb
 
 ENV_NAME = 'Digger-v0'
 MAP_NAME = '2x2_e'
+JOB_TYPE = '6-values'
+
+SEQUENTIAL_MEMORY = 50000
+WINDOW_LENGTH = 1
+STEPS_WARMUP = 10
+TARGET_MODEL_UPDATE = 1e-2
+LEARNING_RATE = 1e-3
+STEPS = 5000
 
 wandb.init(project='Macbook-Local-Run',
            group=MAP_NAME,
-           job_type='6-values',
+           job_type=JOB_TYPE,
            settings=wandb.Settings(start_method="thread"))
 
 # Get the environment and extract the number of actions.
@@ -39,16 +47,16 @@ print(model.summary())
 
 # Finally, we configure and compile our agent. You can use every built-in tensorflow.keras optimizer and
 # even the metrics!
-memory = SequentialMemory(limit=50000, window_length=1)
+memory = SequentialMemory(limit=SEQUENTIAL_MEMORY, window_length=WINDOW_LENGTH)
 policy = BoltzmannQPolicy()
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
-               target_model_update=1e-2, policy=policy)
-dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=STEPS_WARMUP,
+               target_model_update=TARGET_MODEL_UPDATE, policy=policy)
+dqn.compile(Adam(lr=LEARNING_RATE), metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=50000, visualize=False, verbose=2, callbacks=[WandbLogger(), FileLogger('results.json')])
+dqn.fit(env, nb_steps=STEPS, visualize=False, verbose=2, callbacks=[WandbLogger(), FileLogger('results.json')])
 
 # After training is done, we save the final weights.
 dqn.save_weights(f'dqn_{ENV_NAME}_{MAP_NAME}_weights.h5f', overwrite=True)
